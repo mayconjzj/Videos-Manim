d9d6dfb8f890535412171ba5b24edacef262bb96
Maycon Douglas <mayconxrz@gmail.com>
2026-08-27 11:47:45 -0400

    Commit

--- video1.py
+++ video1.py
@@ -18,10 +18,8 @@ class SeriesMatematicas(Scene):
             Séries Matemáticas \\
             """,
                 font_size=48,
-            ).set_color_by_gradient(BLUE, GREEN, YELLOW, ORANGE, RED),
-            MathTex(r"\sum_{n=1}^{\infty} a_n").set_color_by_gradient(
-                BLUE, GREEN, YELLOW, ORANGE, RED
             ),
+            MathTex(r"\sum_{n=1}^{\infty} a_n"),
         ).arrange(DOWN, buff=0.35)
 
         self.play(Write(titulo), run_time=2)
@@ -35,10 +33,8 @@ class SeriesMatematicas(Scene):
             Dada uma sequência de números \\
             reais $(a_n)$
             """
-            ).set_color_by_gradient(BLUE, GREEN, YELLOW, ORANGE, RED),
-            MathTex(r"(a_n) = a_1, a_2, a_3, \ldots, a_n").set_color_by_gradient(
-                BLUE, GREEN, YELLOW, ORANGE, RED
             ),
+            MathTex(r"(a_n) = a_1, a_2, a_3, \ldots, a_n"),
         ).arrange(DOWN, buff=0.35)
 
         self.play(Write(texto1), run_time=3)
@@ -52,20 +48,12 @@ class SeriesMatematicas(Scene):
             A partir dela formamos uma nova \\
             sequência $(S_n)$ onde
             """
-            ).set_color_by_gradient(BLUE, GREEN, YELLOW, ORANGE, RED),
+            ),
             VGroup(
-                MathTex(r"S_1 = a_1").set_color_by_gradient(
-                    BLUE, GREEN, YELLOW, ORANGE, RED
-                ),
-                MathTex(r"S_2 = a_1 + a_2").set_color_by_gradient(
-                    BLUE, GREEN, YELLOW, ORANGE, RED
-                ),
-                MathTex(r"\vdots").set_color_by_gradient(
-                    BLUE, GREEN, YELLOW, ORANGE, RED
-                ),
-                MathTex(r"S_n = \sum_{i=1}^n a_i").set_color_by_gradient(
-                    BLUE, GREEN, YELLOW, ORANGE, RED
-                ),
+                MathTex(r"S_1 = a_1"),
+                MathTex(r"S_2 = a_1 + a_2"),
+                MathTex(r"\vdots"),
+                MathTex(r"S_n = \sum_{i=1}^n a_i"),
             ).arrange(DOWN, buff=0.35),
         ).arrange(DOWN, buff=1)
 
@@ -80,17 +68,69 @@ class SeriesMatematicas(Scene):
                 Os números $S_n$ são chamados de \\
                 \textbf{somas parciais} da série $\sum a_n$
                 """
-            ).set_color_by_gradient(BLUE, GREEN, YELLOW, ORANGE, RED),
+            ),
             Tex(
                 r"""
                 A parcela $a_n$ é o n-ésimo termo \\
                 ou termo geral da série.
                 """
-            ).set_color_by_gradient(BLUE, GREEN, YELLOW, ORANGE, RED),
+            ),
         ).arrange(DOWN, buff=0.35)
 
         self.play(Write(texto3), run_time=4)
         self.wait(2)
         self.play(FadeOut(texto3))
 
-        self.wait(5)
+        texto4 = VGroup(
+            Tex(
+                r"""
+                Se exister o limite $\lim_{n \to \infty} S_n$, \\
+                diremos que a série $\sum a_n$ é \\
+                \textbf{convergente} e \\
+                $S = \sum a_n = \sum_{n = 1}^{\infty} a_n = a_1 + a_2 + $\\
+                $\ldots + a_n + \ldots$ \\
+                será chamado a soma da série. \\
+                Se o $\lim_{n \to \infty} S_n$ não existir, \\
+                diremos que $\sum a_n$ é uma série \\
+                \textbf{divergente}.
+                """
+            ),
+        ).arrange(DOWN, buff=0.35)
+
+        self.play(Write(texto4), run_time=6)
+        self.wait(4)
+        self.play(FadeOut(texto4))
+
+        texto5 = VGroup(
+            Tex(
+                r"""
+                Em outras palavras, a série $\sum a_n$ \\
+                é convergente se a sequência de \\
+                somas parciais $(S_n)$ for convergente.
+                """
+            ),
+        ).arrange(DOWN, buff=0.35)
+
+        self.play(Write(texto5), run_time=4)
+        self.wait(2)
+        self.play(FadeOut(texto5))
+
+        texto6 = VGroup(
+            Tex(
+                r"""
+                Exemplo:
+                """,
+                font_size=48,
+            ),
+            Tex(
+                r"""
+                A série $\sum_{n=1}^{\infty} \frac{1}{n^2}$ é convergente, \\
+                pois a sequência de somas parciais $(S_n)$ \\
+                converge para $\frac{\pi^2}{6}$.
+                """
+            ),
+        ).arrange(DOWN, buff=0.35)
+
+        self.play(Write(texto6), run_time=4)
+        self.wait(4)
+        self.play(FadeOut(texto6))
